e0a6989f55599e5550a320df6e32b058e7d5af2f
Flavio <flavio.ssiv@gmail.com>
2018-11-15 19:22:12 -0300

    finished header styling

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,25 +2,25 @@ var gulp = require('gulp');
 var watch = require('gulp-watch');
 var browserSync = require('browser-sync').create();
 
-gulp.task('watch', function () {
+gulp.task('watch', function() {
 
-  browserSync.init({
-    notify: false,
-    server: {
-      baseDir: "app"
-    }
-  })
-  watch('./app/*.html', function () {
-    browserSync.reload();
-  });
+    browserSync.init({
+        notify: false,
+        server: {
+            baseDir: "app"
+        }
+    });
+    watch('./app/*.html', function() {
+        browserSync.reload();
+    });
 
-  watch('./app/assets/**/*css', function () {
-    gulp.start('cssInject');
+    watch('./app/assets/**/*css', function() {
+        gulp.start('cssInject');
 
-  })
+    })
 });
 
-gulp.task('cssInject', ['styles'], function () {
-  return gulp.src('./app/temp/styles/styles.css')
-    .pipe(browserSync.stream());
+gulp.task('cssInject', ['styles'], function() {
+    return gulp.src('./app/temp/styles/styles.css')
+        .pipe(browserSync.stream());
 });
