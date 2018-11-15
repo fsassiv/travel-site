081a5987236e457d0b98f535035a834b97a8912b
Flavio <flavio.ssiv@gmail.com>
2018-11-15 00:01:28 -0300

    Finished responsive images

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,15 +4,16 @@ var autoprefixer = require('autoprefixer');
 var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
 var cssimport = require('postcss-import');
+var mixins = require('postcss-mixins');
 
-gulp.task('styles', function () {
-  console.log('CSS files has been changed...');
-  return gulp.src('./app/assets/styles/styles.css')
-    // .pipe(postCSS([autoprefixer]))
-    .pipe(postCSS([cssimport, cssvars, nested]))
-    .on('error', function (errorInfo) {
-      console.log(errorInfo.toString());
-      this.emit('end');
-    })
-    .pipe(gulp.dest('./app/temp/styles'));
+gulp.task('styles', function() {
+    console.log('CSS files has been changed...');
+    return gulp.src('./app/assets/styles/styles.css')
+        // .pipe(postCSS([autoprefixer]))
+        .pipe(postCSS([cssimport, mixins, cssvars, nested]))
+        .on('error', function(errorInfo) {
+            console.log(errorInfo.toString());
+            this.emit('end');
+        })
+        .pipe(gulp.dest('./app/temp/styles'));
 });
