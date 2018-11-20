d2bf0342eb174083b266e8548b179bb378dd7daa
Flavio <flavio.ssiv@gmail.com>
2018-11-20 10:26:34 -0300

    Finished Mobile menu icon

--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,9 @@ require('./gulp/tasks/styles');
 require('./gulp/tasks/watch');
 require('./gulp/tasks/sprites');
 require('./gulp/tasks/scripts');
+
+var gulp = require('gulp');
+
+gulp.task('default', function () {
+  console.log('Gulp says Hi...');
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-
+var nodeExternals = require('webpack-node-externals');
 module.exports = {
   entry: './app/assets/scripts/app.js',
   mode: 'development',
@@ -7,6 +7,11 @@ module.exports = {
     filename: 'app.js',
     path: path.resolve(__dirname, 'app/temp/scripts')
   },
+  devtool: 'eval-source-map',
+  target: 'node', // in order to ignore built-in modules like path, fs, etc.
+  externals: [nodeExternals({
+    whitelist: ['jquery']
+  })], // in order to ignore all modules in node_modules folder
   module: {
     rules: [{
       test: /\.m?js$/,
