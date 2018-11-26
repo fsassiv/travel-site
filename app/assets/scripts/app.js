82fac73adbd4c320fbed3b97162f897ea4bf6a8b
Flavio <flavio.ssiv@gmail.com>
2018-11-26 09:42:57 -0300

    Sticky header...

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,7 +1,10 @@
 import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 
 var mm = new MobileMenu();
 new RevealOnScroll($('.feature-item'), '85%');
 new RevealOnScroll($('.testimonials'), '60%');
+
+var stickyHeader = new StickyHeader();
